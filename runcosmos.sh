0ee2f9f7d5838d20f2f065a707e6cec0ba592d8e
Aryan Blaauw <ablaauw@gmx.net>
2020-09-01 14:20:51 +0200

    Add files via upload

---
+++ runcosmos.sh
@@ -0,0 +1,12 @@
+echo " Starting thread 1 on GPU 1 "
+./benchmarks --proc-opencl-single --enable-normalization --device-id=0 -m=-1 --measure-output=/var/log/qrack/"measured_cosmos_2004_k80a1_$(date +%F_%R_%S).log" [cosmos] >> k80a1.txt &
+sleep 2
+echo " Starting thread 2 on GPU 1 "
+./benchmarks --proc-opencl-single --enable-normalization --device-id=0 -m=-1 --measure-output=/var/log/qrack/"measured_cosmos_2004_k80a2_$(date +%F_%R_%S).log" [cosmos] >> k80a2.txt &
+sleep 2
+echo " Starting thread 1 on GPU 2 "
+./benchmarks --proc-opencl-single --enable-normalization --device-id=1 -m=-1 --measure-output=/var/log/qrack/"measured_cosmos_2004_k80b1_$(date +%F_%R_%S).log" [cosmos] >> k80b1.txt &
+sleep 2
+echo " Starting thread 2 on GPU 2 "
+./benchmarks --proc-opencl-single --enable-normalization --device-id=1 -m=-1 --measure-output=/var/log/qrack/"measured_cosmos_2004_k80b2_$(date +%F_%R_%S).log" [cosmos] >> k80b2.txt &
+watch -n1 nvidia-smi
