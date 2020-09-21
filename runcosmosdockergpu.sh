5ef225ad712164aa6bd3e547a86dde7eafe99299
Aryan Blaauw <ablaauw@gmx.net>
2020-09-21 08:31:27 +0000

    Update runcosmosdockergpu.sh

--- runcosmosdockergpu.sh
+++ runcosmosdockergpu.sh
@@ -6,7 +6,10 @@
 docker pull twobombs/qracknet:cosmos-gpu1
 docker pull twobombs/qracknet:cosmos-gpu2
 
-docker rm $(docker ps -aq)
+docker rm cosmosgpu1a
+docker rm cosmosgpu2a
+docker rm cosmosgpu1b
+docker rm cosmosgpu2b
 
 docker run --name cosmosgpu1a --gpus all --device=/dev/dri:/dev/dri -d --mount type=bind,source=/var/log/qrack/1,target=/var/log/qrack twobombs/qracknet:cosmos-gpu1
 docker run --name cosmosgpu2a --gpus all --device=/dev/dri:/dev/dri -d --mount type=bind,source=/var/log/qrack/2,target=/var/log/qrack twobombs/qracknet:cosmos-gpu2
@@ -14,11 +17,6 @@ docker run --name cosmosgpu1b --gpus all --device=/dev/dri:/dev/dri -d --mount t
 docker run --name cosmosgpu2b --gpus all --device=/dev/dri:/dev/dri -d --mount type=bind,source=/var/log/qrack/4,target=/var/log/qrack twobombs/qracknet:cosmos-gpu2
 
 docker logs --follow cosmosgpu2b
-
-docker rm cosmosgpu1a
-docker rm cosmosgpu2a
-docker rm cosmosgpu1b
-docker rm cosmosgpu2b
 
 # run again
 ./runcosmosdockergpu.sh
