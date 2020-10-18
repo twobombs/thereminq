0cd1e82efec598f1bcd1c7f7da161b3a0062e1d4
Aryan Blaauw <aryan@brixcrm.nl>
2020-10-18 11:16:58 +0200

    switch to qft

--- runcosmosdockergpu.sh
+++ runcosmosdockergpu.sh
@@ -1,6 +1,6 @@
 #
 # example: when a controller image had docker enabled through a docker socket fs mount, the controller can call docker worker instances.
-#
+# in this example 4 GPU instances are started to run on a 2 GPU system, after completion a pause of 120 seconds sets in before starting again
 #
 
 docker pull twobombs/qracknet:cosmos-gpu1
@@ -17,6 +17,8 @@ docker run --name cosmosgpu1b --gpus all --device=/dev/dri:/dev/dri -d --mount t
 docker run --name cosmosgpu2b --gpus all --device=/dev/dri:/dev/dri -d --mount type=bind,source=/var/log/qrack/4,target=/var/log/qrack twobombs/qracknet:cosmos-gpu2
 
 docker logs --follow cosmosgpu2b
+
+sleep 120
 
 # run again
 ./runcosmosdockergpu.sh
