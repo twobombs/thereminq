770c3cdff2364c6da78f97d6a3204bdb09d93bf6
Aryan Blaauw <aryan@brixcrm.nl>
2020-10-20 09:38:31 +0200

    upscale qbit count and iterations of nbody feed

---
+++ runnbody.sh
@@ -0,0 +1,12 @@
+#
+# example: when a controller image had docker enabled through a docker socket fs mount, the controller can call docker worker instances.
+# in this example 2 CPU instances are started and re-invoked until the system is fully stuffed 
+#
+
+docker pull twobombs/qracknet:cosmos-nbody-cpu
+
+docker run -d --mount type=bind,source=/var/log/qrack/,target=/var/log/qrack --mount type=bind,source=/root/.qrack/,target=/root/.qrack twobombs/qracknet:cosmos-nbody-cpu
+docker run -d --mount type=bind,source=/var/log/qrack/,target=/var/log/qrack --mount type=bind,source=/root/.qrack/,target=/root/.qrack twobombs/qracknet:cosmos-nbody-cpu
+
+# run again
+./runnbody.sh
