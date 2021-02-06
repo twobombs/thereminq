799ad5983b7394c9a80f66fce48921c0bc4d9168
Aryan Blaauw <ablaauw@gmx.net>
2021-02-06 14:05:10 +0100

    condensed - no debug

--- convert_measured_values.sh
+++ convert_measured_values.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# collect all qrack output log file and make one file
+# collect all qrack output log file and make one decimal file
 cd /var/log/qrack
-find . -type f -name '*.log' -exec cat {} +  > measured.file
 
-# filter header legenda
-cat measured.file | grep test > measured.values
-
-# replace comma with enter
-cat measured.values | tr , '\n' > measured.lines
-
-# remove test name, now only decimal values are in the list
-cat measured.lines | grep -v test > measured.decimals
+find . -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep -v test > measured.decimals
 
 # convert dec to hex values
 printf '%08X\n' $(< measured.decimals)
