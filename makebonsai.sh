71c88fcc684536b39ccebf980fce9a87bafe72e4
Aryan Blaauw <ablaauw@gmx.net>
2021-02-07 15:15:22 +0100

    Update makebonsai.sh

--- makebonsai.sh
+++ makebonsai.sh
@@ -11,6 +11,16 @@ rows=$(<rows.dec)
 echo $(( rows / 2 )) > points.dec
 printf '%08X\n' $(< points.dec) > points.hex
 
+# dim a square
+points=$(<points.dec)
+pointssqr=$(echo "$points" | awk '{print sqrt($1)}')
+square=$( echo $pointssqr | awk '{printf "%.0f\n", $1}')
+
+echo $square
+
+# create array with coordinates
+seq 0 $square > square.dec
+
 # add header data in front of measured data
 cat points.hex measured.hex > bonsai.hex
 
