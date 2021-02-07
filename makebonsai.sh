180a85e9c5088f9385f0ed03887b50cf2e6472e6
Aryan Blaauw <ablaauw@gmx.net>
2021-02-07 13:55:45 +0100

    Update makebonsai.sh

--- makebonsai.sh
+++ makebonsai.sh
@@ -3,5 +3,16 @@
 # convert logfiles into ascii hex
 ./convert_measured_values.sh > measured.hex
 
-# make ascii hex bin
-xxd -r -p measured.hex measured.bin
+# count the total amount of lines
+wc -l measured.hex | tr " " "\n"| grep -v measured.hex > rows.dec
+
+# derive the amount of points for array dimension declaration
+rows=$(<rows.dec)
+echo $(( rows / 2 )) > points.dec
+printf '%08X\n' $(< points.dec) > points.hex
+
+# add header data in front of measured data
+cat points.hex measured.hex > bonsai.hex
+
+# make hex string data a bin file
+xxd -r -p bonsai.hex bonsai.bin
