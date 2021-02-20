8fafd93474e1b0e02fa34dbb9ac5e9829c4f525f
Aryan Blaauw <ablaauw@gmx.net>
2021-02-20 17:26:24 +0100

    multi build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,21 +9,21 @@ docker build -t twobombs/qracknet:cpu -f ./Dockerfile-cpu .
 docker build -t twobombs/qracknet:cosmos-cpu -f ./Dockerfile-cosmos-cpu .
 docker build -t twobombs/qracknet:cosmos-gpu1 -f ./Dockerfile-cosmos-gpu1 .
 docker build -t twobombs/qracknet:cosmos-gpu2 -f ./Dockerfile-cosmos-gpu2 .
-docker build -t twobombs/qracknet:cosmos-gpu3 -f ./Dockerfile-cosmos-gpu3 .
+docker build -t twobombs/qracknet:cosmos-multi -f ./Dockerfile-cosmos-multi .
 
 docker build -t twobombs/qracknet:cosmos-nbody-cpu -f ./Dockerfile-cosmos-nbody-cpu .
 docker build -t twobombs/qracknet:cosmos-nbody-gpu1 -f ./Dockerfile-cosmos-nbody-gpu1 .
 docker build -t twobombs/qracknet:cosmos-nbody-gpu2 -f ./Dockerfile-cosmos-nbody-gpu2 .
-docker build -t twobombs/qracknet:cosmos-nbody-gpu3 -f ./Dockerfile-cosmos-nbody-gpu3 .
+docker build -t twobombs/qracknet:cosmos-nbody-multi -f ./Dockerfile-cosmos-nbody-multi .
 
 docker build -t twobombs/qracknet:qft-cpu -f ./Dockerfile-qft-cpu .
 docker build -t twobombs/qracknet:qft-gpu1 -f ./Dockerfile-qft-gpu1 .
 docker build -t twobombs/qracknet:qft-gpu2 -f ./Dockerfile-qft-gpu2 .
-docker build -t twobombs/qracknet:qft-gpu3 -f ./Dockerfile-qft-gpu3 .
+docker build -t twobombs/qracknet:qft-multi -f ./Dockerfile-qft-multi .
 
 docker build -t twobombs/qracknet:supreme-cpu -f ./Dockerfile-supreme-cpu .
 docker build -t twobombs/qracknet:supreme-gpu1 -f ./Dockerfile-supreme-gpu1 .
 docker build -t twobombs/qracknet:supreme-gpu2 -f ./Dockerfile-supreme-gpu2 .
-docker build -t twobombs/qracknet:supreme-gpu3 -f ./Dockerfile-supreme-gpu3 .
+docker build -t twobombs/qracknet:supreme-multi -f ./Dockerfile-supreme-multi .
 
 docker build -t twobombs/qracknet:dummyload -f ./Dockerfile-dummyload .
