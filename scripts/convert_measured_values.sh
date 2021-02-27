fa573374895607968cc1987fc3dd3a35cfa3b179
Aryan Blaauw <aryan@brixcrm.nl>
2021-02-27 11:30:20 +0100

    added: qubit filter, displacement vectors, fixes

--- scripts/convert_measured_values.sh
+++ scripts/convert_measured_values.sh
@@ -3,7 +3,7 @@
 # collect all qrack output log file and make one decimal file
 cd /var/log/qrack
 
-find . -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep -v test | grep -v Binary > measured.decimals
+find . -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep 26 | grep -v test | grep -v Binary > measured.decimals
 
 # convert dec to hex values
 printf '%08X\n' $(< measured.decimals)
--- scripts/makebonsai.sh
+++ scripts/makebonsai.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# convert logfiles into ascii hex
+# convert logfiles into ascii hex and split in q and m
 ./convert_measured_values.sh > measured.hex
+awk '{ print > (NR % 2 ? "measuredq.hex" : "measuredm.hex") }' measured.hex
 
 # count the total amount of lines with values
 wc -l measured.hex | tr " " "\n"| grep -v measured.hex > rows.dec
@@ -16,26 +17,35 @@ points=$(<points.dec)
 pointssqr=$(echo "$points" | awk '{print sqrt($1)}')
 square=$( echo $pointssqr | awk '{printf "%.0f\n", $1}')
 
-echo $square
+# announce and declare the cube dimensions
+echo $square "x" $square
+echo $square > cube.dec
 
 # create default array row and make it hex
 seq 0 $square > square.dec
 printf '%08X\n' $(< square.dec) > square.hex
 
-# make xz coordinates and convert to hex
-for i in {1..square};do cat square.hex; done > square10x.hex
-(awk '{for(j=0; j<=square; j++)print}' square.dec) > square10z.dec
-printf '%08X\n' $(< square10z.dec) > square10z.hex
+# make xz coordinates and convert to hex, delete previous z-axis results
+touch square10z.hex
+rm square10z.hex
+
+for i in `cat square.dec` ; do seq 1 $square | xargs -i -- echo $i; done > square10x.dec
+for i in `cat square.dec` ; do cat square.hex ; done > square10z.hex
+
+printf '%08X\n' $(< square10x.dec) > square10x.hex
+# printf '%08X\n' $(< square10z.dec) > square10z.hex
+
+# add velocity nullpointers for xyz displacements
+yes 00000000 | head -n `cat points.dec` > displacex.hex
+cp displacex.hex displacey.hex
+cp displacex.hex displacez.hex
 
 # assemble/weave final hex, convert to bin
 # add header data in front of measured bin data
 
-paste square10x.hex square10z.hex > bonsaixz.hex
-paste measured.hex measured.hex > bonsaiym.hex
-paste bonsaixz.hex bonsaiym.hex > bonsaixyzm.hex
+paste square10x.hex measuredm.hex square10z.hex measuredq.hex displacex.hex displacey.hex displacez.hex > xyzmxyz.hex
 
-cat points.hex bonsaixyzm.hex > bonsai.hex
-
+cat points.hex xyzmxyz.hex > bonsai.hex
 
 # convert hex string data a bin file
 xxd -r -p bonsai.hex bonsai.bin
