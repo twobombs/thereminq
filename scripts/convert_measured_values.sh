a7f0306a7b70d737026375d474e1ceca8eec25a9
Aryan Blaauw <ablaauw@gmx.net>
2021-04-24 11:24:09 +0200

    Update convert_measured_values.sh

--- scripts/convert_measured_values.sh
+++ scripts/convert_measured_values.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # collect all qrack output log file and make one decimal file
-# cd /var/log/qrack
 
 find /var/log/qrack/. -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep 26 | grep -v test | grep -v Binary > measured.dec
 
