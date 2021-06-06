9f5b56c41fbaea9d0a5d9c7fbd14cad5bd9d989c
Aryan <ablaauw@gmx.net>
2021-06-06 11:29:48 +0200

    added: tipsy scaling

--- scripts/maketipsy.sh
+++ scripts/maketipsy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# this bash script was made with the help of unicorns and fairydust
+# do not touch this code unless you want to change the way this universe works
 
-# convert logfiles into ascii hex and split in q and m
+# convert qrack logfiles into ascii hex and split in q and m
+#
+echo "reading qrack output in /var/log/qrack"
 ./convert_measured_values.sh > measured.hex
 # awk '{ print > (NR % 2 ? "measuredq.hex" : "measuredm.hex") }' measured.hex
 
@@ -33,9 +38,23 @@ square=$((square - margin))
 echo $square > cube.dec
 
 # announce and declare raw points
-# make real cube dimensions and convert value to hex for header
+# make real and tipsy scaling 
+# cube dimensions and convert value to hex for header
+
 echo "original amount of measured values" $points
 echo "view will be "$square "x" $square
+
+# determine coordinate scaling factor 
+tipsymax=4294967295
+tipsy=$(($tipsymax/$square))
+echo "tipsy scaling factor " $tipsy
+
+# determine quantum register scaling factor
+# number of qubits mask is hardcoded to 0xFFFFFFFF, 32 qbits
+tipsyq=$(($tipsymax/32))
+echo "tipsy quantum factor " $tipsyq
+
+# resize number of points via dim and make hex
 echo $((square * square)) > points.dec
 points=$(<points.dec)
 printf '%08X\n' $(< points.dec) > points.hex
@@ -50,7 +69,7 @@ head -n -$delta measured.hex > measured2.hex
 mv measured2.hex measured.hex
 
 # announce result and delta
-echo $delta
+echo "clipped values:" $delta
 wc -l measured.hex
 
 # split in q and m results from clipped measured.hex
@@ -60,17 +79,24 @@ awk '{ print > (NR % 2 ? "measuredq.hex" : "measuredm.hex") }' measured.hex
 seq 0 $square > square.dec
 printf '%08X\n' $(< square.dec) > square.hex
 
+# create tipsy scale array row and make it hex
+seq 0 $tipsy $tipsymax > squaretipsy.dec
+printf '%08X\n' $(< squaretipsy.dec) > squaretipsy.hex
+
 # default ID is 2
 yes 02 | head -n `cat points.dec` > id.hex
 yes 02000000 | head -n `cat points.dec` > idlong.hex
 
-# make xz coordinates, cut offsets, convert to hex
-for i in `cat square.dec` ; do yes $i | head -n $square; done > square10x.dec
+# make x coordinates, cut offsets, convert to hex
+for i in `cat square.dec` ; do yes $(($i*$tipsy)) | head -n $square; done > square10x.dec
 head -n -$square square10x.dec > square10xdelta.dec
 mv square10xdelta.dec square10x.dec
+
+# convert x-coordinates outcome to hex
 printf '%08X\n' $(< square10x.dec) > square10x.hex
 
-for i in `cat square.dec` ; do cat square.hex ; done > square10z.hex
+# make z coordinates in hex on tipsy scale and cut it to size
+for i in `cat square.dec` ; do cat squaretipsy.hex ; done > square10z.hex
 head -n -$square square10z.hex > square10zdelta.hex
 mv square10zdelta.hex square10z.hex
 head -n -$square square10z.hex > square10zdelta.hex
@@ -84,10 +110,7 @@ cp displacex.hex displacey.hex
 cp displacex.hex displacez.hex
 cp displacex.hex dummy.hex
 
-# assemble/weave final hex, convert to bin
-# add header data in front of measured bin data
-
-echo " "
+echo "size checks:"
 cat points.dec
 wc -l square10x.hex
 wc -l measuredm.hex
@@ -99,22 +122,18 @@ wc -l displacex.hex
 wc -l displacey.hex
 wc -l displacez.hex
 
-# convert points.hex from AB to BA format
-# tail -c 5 points.hex > pointsvar.hex
-# head -c 4 points.hex >> pointsvar.hex
-# cat pointsvar.hex |  tr -d "\n" > points.hex
+# assemble/weave final hex, convert to bin
+# add header data in front of measured bin data
 
 paste points.hex ndim.hex nsph.hex ndark.hex points.hex version.hex measuredq.hex square10x.hex measuredm.hex square10z.hex displacex.hex displacey.hex displacez.hex dummy.hex dummy.hex square10x.hex square10z.hex > xyzmxyz.hex
 
 # add time.hex in front of header+data
-
 cat time.hex xyzmxyz.hex > tipsy.hex
 
 # convert hex string data a bin file
 xxd -r -p tipsy.hex tipsy.bin
 # aaaand convert to little indian
 hexdump -v -e '1/4 "%08x"' -e '"\n"' tipsy.bin | xxd -r -p >tipsy_hexdump.bin
-
 
 echo " "
 echo "done"
