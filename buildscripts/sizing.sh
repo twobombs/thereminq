9ecbc74e52f1613736488c46366e0bbcc32dde10
Aryan Blaauw <ablaauw@gmx.net>
2021-07-17 20:35:42 +0200

    sizing

---
+++ buildscripts/sizing.sh
@@ -0,0 +1,24 @@
+cat points.dec
+
+wc -l square10x.hex
+wc -l measuredm.hex
+wc -l square10z.hex
+wc -l measuredq.hex
+
+wc -l square10x.flex
+wc -l measuredm.flex
+wc -l square10z.flex
+wc -l measuredq.flex
+
+
+wc -l square10x.fhex
+wc -l measuredm.fhex
+wc -l square10z.fhex
+wc -l measuredq.fhex
+
+wc -l dummy.hex
+wc -l idlong.hex
+wc -l displacex.hex
+wc -l displacey.hex
+wc -l displacez.hex
+
