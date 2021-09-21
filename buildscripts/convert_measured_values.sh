c33f01b6be12d6ef15c9de507c98e96864727a9c
Aryan Blaauw <ablaauw@gmx.net>
2021-09-21 15:56:26 +0200

    Update convert_measured_values.sh

--- buildscripts/convert_measured_values.sh
+++ buildscripts/convert_measured_values.sh
@@ -2,7 +2,7 @@
 
 # collect all qrack output log file and make one decimal file
 
-find /var/log/qrack/. -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep '27' | grep -v test | grep -v Binary > measured.dec
+find /var/log/qrack/. -type f -name '*.log' -exec cat {} + | grep test | tr , '\n' | grep '7' | grep -v test | grep -v Binary > measured.dec
 
 # convert dec to hex values
 printf '%08X\n' $(< measured.dec)
