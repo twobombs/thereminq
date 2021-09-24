9e3a3afbe8c96719aadfeebd50dc4be6be6e3ace
Aryan Blaauw <ablaauw@gmx.net>
2021-09-24 10:26:07 +0200

    Update sizing.sh

--- buildscripts/sizing.sh
+++ buildscripts/sizing.sh
@@ -10,15 +10,14 @@ wc -l measuredm.flex
 wc -l square10z.flex
 wc -l measuredq.flex
 
-
 wc -l square10x.fhex
 wc -l measuredm.fhex
 wc -l square10z.fhex
 wc -l measuredq.fhex
+wc -l measuredvol.fhex
 
 wc -l dummy.hex
 wc -l idlong.hex
 wc -l displacex.hex
 wc -l displacey.hex
 wc -l displacez.hex
-
