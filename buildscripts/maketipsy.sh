efad8d6d990f34a8ee040c35ff0c6a2150246cba
Aryan Blaauw <ablaauw@gmx.net>
2021-10-14 19:26:54 +0000

    Update maketipsy.sh

--- buildscripts/maketipsy.sh
+++ buildscripts/maketipsy.sh
@@ -45,7 +45,7 @@ echo "original amount of measured values" $points
 echo "view will be "$square "x" $square
 
 # determine coordinate scaling factor 
-tipsymax=33554431
+tipsymax=8388607
 tipsy=$(($tipsymax/$square))
 echo "tipsy maximum factor " $tipsymax
 echo "tipsy scaling factor " $tipsy
