5796e74dd77073ad9119e1af5b2e232ebc13cfbb
Aryan Blaauw <ablaauw@gmx.net>
2021-10-18 20:43:44 +0200

    Update supreme.sh

--- buildscripts/supreme.sh
+++ buildscripts/supreme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # we fetch the sycamore 25 qbit over 14 depth and extract it for filter-feed
-mkdir supreme && cd supreme && wget https://datadryad.org/stash/downloads/file_stream/693524
+mkdir supreme && cd supreme && wget https://datadryad.org/stash/downloads/file_stream/693522
