3dea559a641722d9179d5d2d034210af1cb7a8fd
Aryan Blaauw <ablaauw@gmx.net>
2021-10-25 20:16:44 +0000

    Update supreme.sh

--- buildscripts/supreme.sh
+++ buildscripts/supreme.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
-# we fetch the sycamore 28 qbit over 14 depth and extract it for filter-feed
+# we fetch the sycamore 28 qbit over 14 depth and extract it for filter-feed processing
 wget https://datadryad.org/stash/downloads/file_stream/693522
+tar -xvzf 693522 && cd n28_m14
+data=$(cat measured_supreme_28q14d.txt); for byte in $data; do echo $(printf '%x\n' "$((2#$byte))"); done | xxd -r -p > measured_supreme_28q14d.dec
+printf '%08X\n' $(<measured_supreme_28q14d.dec) > measured_supreme_28q14d.hex
