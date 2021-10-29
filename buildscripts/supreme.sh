fb13cc7a95221f508841abe090e17b0c5c27a2dc
Aryan Blaauw <ablaauw@gmx.net>
2021-10-29 11:07:41 +0200

    Update supreme.sh

--- buildscripts/supreme.sh
+++ buildscripts/supreme.sh
@@ -8,43 +8,43 @@ cat measure* > measured_supreme_28q14d.txt
 # this needs at least 20 cores to execute
 for a in $(< measurements_n28_m14_s0_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s0_e0_pEFGH.flex &
 echo "forked 1"
-for a in $(< measurements_n28_m14_s0_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s0_e6_pEFGH.flex &
-echo "forked 2"
+# for a in $(< measurements_n28_m14_s0_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s0_e6_pEFGH.flex &
+# echo "forked 2"
 for a in $(< measurements_n28_m14_s1_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s1_e0_pEFGH.flex &
 echo "forked 3"
-for a in $(< measurements_n28_m14_s1_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s1_e6_pEFGH.flex &
-echo "forked 4"
+# for a in $(< measurements_n28_m14_s1_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s1_e6_pEFGH.flex &
+# echo "forked 4"
 for a in $(< measurements_n28_m14_s2_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s2_e0_pEFGH.flex &
 echo "forked 5"
-for a in $(< measurements_n28_m14_s2_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s0_e6_pEFGH.flex &
-echo "forked 6"
+# for a in $(< measurements_n28_m14_s2_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s0_e6_pEFGH.flex &
+# echo "forked 6"
 for a in $(< measurements_n28_m14_s3_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s3_e0_pEFGH.flex &
 echo "forked 7"
-for a in $(< measurements_n28_m14_s3_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s3_e6_pEFGH.flex &
-echo "forked 8"
+# for a in $(< measurements_n28_m14_s3_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s3_e6_pEFGH.flex &
+# echo "forked 8"
 for a in $(< measurements_n28_m14_s4_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s4_e0_pEFGH.flex &
 echo "forked 9"
-for a in $(< measurements_n28_m14_s4_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s4_e6_pEFGH.flex &
-echo "forked 10"
+# for a in $(< measurements_n28_m14_s4_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s4_e6_pEFGH.flex &
+# echo "forked 10"
 for a in $(< measurements_n28_m14_s5_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s5_e0_pEFGH.flex &
 echo "forked 11"
-for a in $(< measurements_n28_m14_s5_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s5_e6_pEFGH.flex &
-echo "forked 12"
+# for a in $(< measurements_n28_m14_s5_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s5_e6_pEFGH.flex &
+# echo "forked 12"
 for a in $(< measurements_n28_m14_s6_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s6_e0_pEFGH.flex &
 echo "forked 13"
-for a in $(< measurements_n28_m14_s6_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s6_e6_pEFGH.flex &
-echo "forked 14"
+# for a in $(< measurements_n28_m14_s6_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s6_e6_pEFGH.flex &
+# echo "forked 14"
 for a in $(< measurements_n28_m14_s7_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s7_e0_pEFGH.flex &
 echo "forked 15"
-for a in $(< measurements_n28_m14_s7_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s7_e6_pEFGH.flex &
-echo "forked 16"
+# for a in $(< measurements_n28_m14_s7_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s7_e6_pEFGH.flex &
+# echo "forked 16"
 for a in $(< measurements_n28_m14_s8_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s8_e0_pEFGH.flex &
 echo "forked 17"
-for a in $(< measurements_n28_m14_s8_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s8_e6_pEFGH.flex &
-echo "forked 18"
-for a in $(< measurements_n28_m14_s9_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s9_e0_pEFGH.flex &
+# for a in $(< measurements_n28_m14_s8_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s8_e6_pEFGH.flex &
+# echo "forked 18"
+for a in $(< measurements_n28_m14_s9_e0_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s9_e0_pEFGH.flex
 echo "forked 19"
-for a in $(< measurements_n28_m14_s9_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s9_e6_pEFGH.flex
+# for a in $(< measurements_n28_m14_s9_e6_pEFGH.txt); do /root/.local/bin/crackNum -i28 0b $(echo $a) | grep "Hex layout" ; done > ../n28_m14_s9_e6_pEFGH.flex
 echo "ready"
 
 echo  "60 secs for tasks to finish"
