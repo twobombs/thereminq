94643713c13a6195a047b957b84f32cf99d7bd9a
Aryan Blaauw <ablaauw@gmx.net>
2021-11-21 11:24:31 +0100

    added: sycamore scrape logs

---
+++ buildscripts/scrapelogs.sh
@@ -0,0 +1,23 @@
+# scrape /var/log/qrack logs into resultsfile per number of qubits with 'collapsed wave function' 0 and binary conveniant value filter
+#
+# sycamore results:
+#
+cat measured_supreme_* | grep ',12,' | grep -v 'TestName' | grep -v ',12,0' > supreme_12q14d_results.txt
+cat measured_supreme_* | grep ',14,' | grep -v 'TestName' | grep -v ',14,0' > supreme_14q14d_results.txt
+cat measured_supreme_* | grep ',16,' | grep -v 'TestName' | grep -v ',16,0' > supreme_16q14d_results.txt
+cat measured_supreme_* | grep ',18,' | grep -v 'TestName' | grep -v ',18,0' > supreme_18q14d_results.txt
+cat measured_supreme_* | grep ',20,' | grep -v 'TestName' | grep -v ',20,0' > supreme_20q14d_results.txt
+cat measured_supreme_* | grep ',22,' | grep -v 'TestName' | grep -v ',22,0' > supreme_22q14d_results.txt
+cat measured_supreme_* | grep ',24,' | grep -v 'TestName' | grep -v ',24,0' > supreme_24q14d_results.txt
+cat measured_supreme_* | grep ',26,' | grep -v 'TestName' | grep -v ',26,0' > supreme_26q14d_results.txt
+cat measured_supreme_* | grep ',28,' | grep -v 'TestName' | grep -v ',28,0' > supreme_28q14d_results.txt
+#
+wc -l supreme_12q14d_results.txt
+wc -l supreme_14q14d_results.txt
+wc -l supreme_16q14d_results.txt
+wc -l supreme_18q14d_results.txt
+wc -l supreme_20q14d_results.txt
+wc -l supreme_22q14d_results.txt
+wc -l supreme_24q14d_results.txt
+wc -l supreme_26q14d_results.txt
+wc -l supreme_28q14d_results.txt
