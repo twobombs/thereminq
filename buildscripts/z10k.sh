f7058938f5d07452937a3db73e30c5aa405310a9
Aryan Blaauw <ablaauw@gmx.net>
2021-12-01 09:47:16 +0100

    Update z10k.sh

--- buildscripts/z10k.sh
+++ buildscripts/z10k.sh
@@ -3,5 +3,5 @@
 seq 1 10000 > z10k.dec
 awk '{x=30*-cos($1*0.0003); print x;}' z10k.dec > z10kcos.dec
 echo "converting z10k to float hex"
-for a in $(< z10kcos.dec); do /root/.local/bin/crackNum -f sp -- $(echo $a) | grep "Hex layout" ; done > z10k.flex &
+for a in $(< z10kcos.dec); do /root/.local/bin/crackNum -f sp -- $(echo $a) | grep "Hex layout" ; done > z10k.flex
 grep "Hex layout" z10k.flex | tr -d ' ' | tr -d 'Hexlayout:' > z10k.fhex
