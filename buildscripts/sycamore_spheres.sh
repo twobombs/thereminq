53b9bb6d1e7f1b76707be8e94494f4e3f0b001ec
Aryan Blaauw <ablaauw@gmx.net>
2021-12-08 09:04:15 +0100

    Create sycamore_spheres.sh

---
+++ buildscripts/sycamore_spheres.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# this script converts 2x90k of the s0e0 12q/28q-14d sycamore/qrack results
+# and places those values in a tipsy bubble sphere graph 
+# results of this calculation will be placed in the qracknet-graph repo
+# 
+# ==== here starts tipsy header declarations ======
+
+# T = time
+echo "0000000000000000" > time.hex
+echo "00030000" > ndim.hex
+echo "00000000" > nsph.hex
+echo "00000000" > ndark.hex
+echo "00010000" > version.hex
+
+# ==== here end tipsy special declarations =========
+
+# we fetch the sycamore 12 to 28 qbit over 14 depth results and extract it for filter-feed processing
+#
+tar -xvzf /qracknet/miscfiles/qrack-supreme12-28q14d10k.tar.gz ./ && cd qrack-supreme12-28q14d10k
+#
