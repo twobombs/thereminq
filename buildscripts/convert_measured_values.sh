0273cdfb1d993ffaebc7ebc10e186328b26256d6
Aryan Blaauw <ablaauw@gmx.net>
2022-02-13 09:47:21 +0000

    filter -1 values

--- buildscripts/convert_measured_values.sh
+++ buildscripts/convert_measured_values.sh
@@ -2,7 +2,7 @@
 
 # collect all qrack output log file and make one decimal file
 
-find /var/log/qrack/. -type f -name '*.log' -exec cat {} + | grep test | grep ',24,' | grep -v ',24,0' | tr , '\n' | grep -v test | grep -v Binary > measured.dec
+find /var/log/qrack/. -type f -name '*.log' -exec cat {} + | grep test | grep ',24,' | grep -v ',24,0' | grep -v ',24,-1' | tr , '\n' | grep -v test | grep -v Binary > measured.dec
 
 # convert dec to hex values
 printf '%08X\n' $(< measured.dec)
