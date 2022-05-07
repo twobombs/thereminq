56d73a9f6cad60b7a033073761b6f43ff0d8314c
Aryan Blaauw <ablaauw@gmx.net>
2022-05-07 20:54:54 +0200

    Update shors_winloop.sh

--- buildscripts/shors_winloop.sh
+++ buildscripts/shors_winloop.sh
@@ -1,3 +1,18 @@
-for i in {16..55108}
-    do prime=$(matho-primes -c 1 -u $i) && echo $(($prime * 128)) | ./shors_factoring $1 | grep "Found"
+for i in {1..55108}
+    do
+	for j in {1..10}
+	do
+	prime=$(matho-primes -c 1 -u $i)
+	echo $(($prime * $prime)) > i2
+	i2=$(<i2)
+	prime2=$(matho-primes -c 1 -u $i2)
+	echo $(($prime * $prime2)) > fact
+	fact=$(<fact)
+	echo $fact | ./shors_factoring $1 > factoring
+	cat factoring | grep Found
+	cat factoring | grep Failed
+	cat factoring | grep 'non- relative prime'
+	echo 'Prime #1:   ' $prime 'Prime #2:   ' $prime2 'Multiplied: ' $fact
+	echo ' '
+    done
 done
