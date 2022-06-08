93dad1b45931631922af25dc2aa1b622a70f7723
Aryan Blaauw <ablaauw@gmx.net>
2022-06-08 21:42:44 +0000

    Update fetch-primes.sh

--- buildscripts/fetch-primes.sh
+++ buildscripts/fetch-primes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this little puppy will fetch the compressed dataset of the first 2 Billion primes and convert it for Elastic/Qimcifa consumption.
+# this little puppy will fetch the compressed dataset of the last 200 million primes up to 1T in primes and convert it for Elastic/Qimcifa consumption.
 # It will eat 30Gb. it's called BigData for a reason and we've not even arrived at BigINT yet ( max here is 36 bits )  
 #
 apt update && apt install -y p7zip
