d21e5cf852b8a693e0dd55c7ed1617a8e78f3502
Aryan Blaauw <ablaauw@gmx.net>
2022-06-08 19:49:00 +0000

    Create run-fetch-primes.sh

---
+++ runscripts/run-fetch-primes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update && apt install -y p7zip
+
+mkdir primes
+cd primes
+
+for i in {1..200}
+ do
+  wget 'http://www.primos.mat.br/dados/2T_part'$i'.7z'
+ done
+
+p7zip -d *.7z
