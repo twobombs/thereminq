818deba22703c5ad20d78fa6adeb7392c2d9afb5
Aryan Blaauw <ablaauw@gmx.net>
2022-06-12 20:51:34 +0200

    prime fetch+

--- buildscripts/0-fetch-primes.sh
+++ buildscripts/0-fetch-primes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#
 # this little puppy will fetch the compressed dataset of the last 200 million primes up to 1T in primes and convert it for Elastic/Qimcifa consumption.
-# It will eat 30Gb. it's called BigData for a reason and we've not even arrived at BigINT yet ( max here is 36 bits )  
+# It will eat 30Gb. it's called BigData for a reason and we've not even arrived at BigINT yet ( max here is 'only' 36 bits )  
 #
 apt update && apt install -y p7zip
 
@@ -9,7 +10,7 @@ cd primes
 
 for i in {1..200}
  do
-  wget 'http://www.primos.mat.br/dados/De_900G_a_1T_part'$i'.7z'
+  wget 'http://www.primos.mat.br/dados/2T_part'$i'.7z'
  done
 
 p7zip -d *.7z
---
+++ buildscripts/1-filter-primes.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+# transfer 200M primes into /var/log/sieve
+#
+mkdir /var/log/sieve
+#
+# this wil generate a load of 200+ for 120 mins
+# this script will fork 200 prime list generators
+# and place them in /log for filebeat to pick up
+# don't do this on 24 cores or less with ssd
+#
+for i in {1..199}
+    do
+	echo $i
+	cat 2T_part$i* | sed 's/\s\+/\n/g' > /var/log/sieve/primes${i}.txt &
+    done
