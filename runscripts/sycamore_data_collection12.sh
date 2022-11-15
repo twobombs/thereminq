1c8c58a3c2c4ba2a90ff774aa895e726c99783f0
Aryan Blaauw <ablaauw@gmx.net>
2022-11-15 19:48:35 +0100

    Add files via upload

---
+++ runscripts/sycamore_data_collection12.sh
@@ -0,0 +1,33 @@
+export  QRACK_OCL_DEFAULT_DEVICE=4
+
+export QRACK_QPAGER_DEVICES=11.1,11.2,11.3,11.1
+export QRACK_QPAGER_DEVICES_HOST_POINTER=11.0,11,1
+export QRACK_MAX_ALLOC_MB=64000
+export QRACK_MAX_PAGING_QB=32
+export QRACK_SEGMENT_GLOBAL_QB=2
+export QRACK_MAX_CPU_QB=32
+
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.45
+./benchmarks --optimal --single --max-qubits=54 --samples=100 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" 
+
+
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.45
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_45_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.40
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_40_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.35
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_35_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.30
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_30_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.25
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_25_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.20
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_20_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.15
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_15_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.10
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_10_12a.csv
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.05
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_05_12a.csv
+unset QRACK_QUNIT_SEPARABILITY_THRESHOLD
+./benchmarks --optimal-single --single --max-qubits=54 --samples=1000 --benchmark-depth=12 test_quantum_supremacy --measure-output=/var/log/qrack/"sycamore_$(date +%F_%R_%S).log" >> q0_00_12a.csv
