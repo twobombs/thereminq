eb022248c7c9d27898de7cf9085fd7f485913d03
Aryan Blaauw <ablaauw@gmx.net>
2022-12-24 12:37:18 +0100

    Update extractboiler.sh

--- buildscripts/extractboiler.sh
+++ buildscripts/extractboiler.sh
@@ -4,8 +4,9 @@
 for i in {4..32}
 
 do
-    cat t_cc_nn_boil$(($i))* | grep -v ',-1' | grep -v 'Name' > /root/boiler$(($i))
-    echo -n $i' - '
-    cat /root/boiler$(($i)) | wc -l
-done
+    cat t_cc_nn_boil$(($i))* | grep -v ',-1' | grep -v ',0' | grep -v 'Name' | tr , '\n' | grep -v test | grep -v Binary > /root/boiler$(($i))
+    printf '%08X\n' $(< /root/boiler$(($i)) ) > /root/boiler$(($i)).hex
 
+    echo -n $i' - '
+    cat /root/boiler$(($i)).hex | grep 00000020 | wc -l
+done
