1ebb1427c662161b2ae61efd8771aec49d8b962e
Aryan Blaauw <ablaauw@gmx.net>
2023-02-11 13:15:27 +0100

    Add files via upload

---
+++ runscripts/nvmeswap.sh
@@ -0,0 +1,5 @@
+zpool create nvme /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1 /dev/nvme5n1 /dev/nvme6n1 -f
+zfs create -V 1.4T -b 8192 -o compression=zstd-19 -o logbias=throughput -o sync=always -o primarycache=metadata -o secondarycache=none -o com.sun:auto-snapshot=false nvme/swap
+mkswap -f /dev/zvol/nvme/swap
+swapon /dev/zvol/nvme/swap
+zfs set compression=on nvme
