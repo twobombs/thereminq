5cad0384740049b55c9a0cd4c781652b31e1dcef
Aryan Blaauw <ablaauw@gmx.net>
2023-03-12 09:27:16 +0100

    Update buildvramfs.sh

--- systemscripts/buildvramfs.sh
+++ systemscripts/buildvramfs.sh
@@ -2,6 +2,6 @@
 # this script will build vramfs that will be invoked by some of the swap vram buffer scripts 
 
 apt install -y flex libncurses-dev bison libelf-dev libssl-dev linux-source make build-essential
-aptt install -y libfuse3-dev opencl-headers ocl-icd-opencl-dev 
+apt install -y libfuse3-dev opencl-headers ocl-icd-opencl-dev 
 apt clean all && ln /usr/bin/fusermount /usr/bin/fusermount3
 git clone https://github.com/Overv/vramfs.git && cd vramfs && make 
