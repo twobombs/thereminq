48cfc6537814d87baa6acc7d42d1419f286b9b96
Aryan Blaauw <ablaauw@gmx.net>
2023-04-02 12:07:28 +0200

    Update build_dockerfiles.sh

--- buildscripts/build_dockerfiles.sh
+++ buildscripts/build_dockerfiles.sh
@@ -1,35 +1,35 @@
 # build qracknet image and its required services
 
-# docker build --no-cache -t twobombs/qracknet:latest ./
+# docker build --no-cache -t twobombs/thereminq:latest ./
 
 cd .. 
 
-docker build -t twobombs/qracknet:latest ./
+docker build -t twobombs/thereminq:latest ./
 
-docker build -t twobombs/qracknet:controller -f ./Dockerfile-controller .
-docker build -t twobombs/qracknet:cpu -f ./Dockerfile-cpu .
+docker build -t twobombs/thereminq:controller -f ./Dockerfile-controller .
+docker build -t twobombs/thereminq:cpu -f ./Dockerfile-cpu .
 
-docker build -t twobombs/qracknet:cosmos-cpu -f ./Dockerfile-cosmos-cpu .
-docker build -t twobombs/qracknet:cosmos-gpu1 -f ./Dockerfile-cosmos-gpu1 .
-docker build -t twobombs/qracknet:cosmos-gpu2 -f ./Dockerfile-cosmos-gpu2 .
-docker build -t twobombs/qracknet:cosmos-multi -f ./Dockerfile-cosmos-multi .
+docker build -t twobombs/thereminq:cosmos-cpu -f ./Dockerfile-cosmos-cpu .
+docker build -t twobombs/thereminq:cosmos-gpu1 -f ./Dockerfile-cosmos-gpu1 .
+docker build -t twobombs/thereminq:cosmos-gpu2 -f ./Dockerfile-cosmos-gpu2 .
+docker build -t twobombs/thereminq:cosmos-multi -f ./Dockerfile-cosmos-multi .
 
-docker build -t twobombs/qracknet:cosmos-nbody-cpu -f ./Dockerfile-cosmos-nbody-cpu .
-docker build -t twobombs/qracknet:cosmos-nbody-gpu1 -f ./Dockerfile-cosmos-nbody-gpu1 .
-docker build -t twobombs/qracknet:cosmos-nbody-gpu2 -f ./Dockerfile-cosmos-nbody-gpu2 .
-docker build -t twobombs/qracknet:cosmos-nbody-multi -f ./Dockerfile-cosmos-nbody-multi .
+docker build -t twobombs/thereminq:cosmos-nbody-cpu -f ./Dockerfile-cosmos-nbody-cpu .
+docker build -t twobombs/thereminq:cosmos-nbody-gpu1 -f ./Dockerfile-cosmos-nbody-gpu1 .
+docker build -t twobombs/thereminq:cosmos-nbody-gpu2 -f ./Dockerfile-cosmos-nbody-gpu2 .
+docker build -t twobombs/thereminq:cosmos-nbody-multi -f ./Dockerfile-cosmos-nbody-multi .
 
-docker build -t twobombs/qracknet:qft-cpu -f ./Dockerfile-qft-cpu .
-docker build -t twobombs/qracknet:qft-gpu1 -f ./Dockerfile-qft-gpu1 .
-docker build -t twobombs/qracknet:qft-gpu2 -f ./Dockerfile-qft-gpu2 .
-docker build -t twobombs/qracknet:qft-multi -f ./Dockerfile-qft-multi .
+docker build -t twobombs/thereminq:qft-cpu -f ./Dockerfile-qft-cpu .
+docker build -t twobombs/thereminq:qft-gpu1 -f ./Dockerfile-qft-gpu1 .
+docker build -t twobombs/thereminq:qft-gpu2 -f ./Dockerfile-qft-gpu2 .
+docker build -t twobombs/thereminq:qft-multi -f ./Dockerfile-qft-multi .
 
-docker build -t twobombs/qracknet:supreme-cpu -f ./Dockerfile-supreme-cpu .
-docker build -t twobombs/qracknet:supreme-gpu1 -f ./Dockerfile-supreme-gpu1 .
-docker build -t twobombs/qracknet:supreme-gpu2 -f ./Dockerfile-supreme-gpu2 .
-docker build -t twobombs/qracknet:supreme-multi -f ./Dockerfile-supreme-multi .
+docker build -t twobombs/thereminq:supreme-cpu -f ./Dockerfile-supreme-cpu .
+docker build -t twobombs/thereminq:supreme-gpu1 -f ./Dockerfile-supreme-gpu1 .
+docker build -t twobombs/thereminq:supreme-gpu2 -f ./Dockerfile-supreme-gpu2 .
+docker build -t twobombs/thereminq:supreme-multi -f ./Dockerfile-supreme-multi .
 
-docker build -t twobombs/qracknet:unittest -f ./Dockerfile-unittest .
-docker build -t twobombs/qracknet:dummyload -f ./Dockerfile-dummyload .
-docker build -t twobombs/qracknet:bonsai -f ./Dockerfile-Bonsai .
-docker build -t twobombs/qracknet:tipsydev -f ./Dockerfile-tipsydev .
+docker build -t twobombs/thereminq:unittest -f ./Dockerfile-unittest .
+docker build -t twobombs/thereminq:dummyload -f ./Dockerfile-dummyload .
+docker build -t twobombs/thereminq:bonsai -f ./Dockerfile-Bonsai .
+docker build -t twobombs/thereminq:tipsydev -f ./Dockerfile-tipsydev .
