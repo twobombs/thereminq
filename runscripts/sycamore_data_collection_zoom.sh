bf1a9be6ea32f791b1b36fd5ededdf8319b66463
Aryan Blaauw <ablaauw@gmx.net>
2023-09-26 18:51:07 +0200

    Update sycamore_data_collection_zoom.sh

--- runscripts/sycamore_data_collection_zoom.sh
+++ runscripts/sycamore_data_collection_zoom.sh
@@ -10,6 +10,6 @@ export QRACK_SEGMENT_GLOBAL_QB=2
 
 export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.35
 
-time ./benchmarks --optimal --single --max-qubits=54 --samples=100 --benchmark-depth=13 test_quantum_supremacy --measure-output=/var/log/qrack/"measured_sycamore53_13_35_$(date +%F_%R_%S).log" >> q0_35_13_zoom.csv
+time ./benchmarks --optimal --measure-shots=1024 --single --max-qubits=54 --samples=100 --benchmark-depth=13 test_quantum_supremacy --measure-output=/var/log/qrack/"measured_sycamore53_13_35_$(date +%F_%R_%S).log" >> q0_35_13_zoom.csv
 
 
