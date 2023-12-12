f252687e9ca6361bc6148505e1759056c3861f2a
Aryan Blaauw <ablaauw@gmx.net>
2023-12-12 18:38:56 +0000

    Update mkswap.sh

--- systemscripts/mkswap.sh
+++ systemscripts/mkswap.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# your cat will catch fire when running this script
+
 mdadm --stop /dev/md127
 mdadm --create --verbose /dev/md127 --level=0 --raid-devices=12 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1 /dev/nvme5n1 /dev/nvme6n1 /dev/nvme7n1 /dev/nvme8n1 /dev/nvme9n1 /dev/nvme10n1 /dev/nvme11n1
 mkfs.ext4 /dev/md127
