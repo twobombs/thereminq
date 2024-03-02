d9331c6fcacde39a7afd4a08317866a4bd8ccc3c
Aryan Blaauw <ablaauw@gmx.net>
2024-03-02 18:23:32 +0100

    Update qrackrng.sh

--- systemscripts/qrackrng.sh
+++ systemscripts/qrackrng.sh
@@ -2,6 +2,7 @@
 #
 # set default device when multigpu; every GPU can generate a stream to be consolidated to /tmp/qrackrng
 export QRACK_OCL_DEFAULT_DEVICE=0
+
 # 24 qubit qft fluctuations are used as rng feed
 ./benchmarks --optimal --measure-shots=1 -m=24 --samples=999999999 --single --measure-output /tmp/test_qft_cosmology.txt test_qft_cosmology &
 
