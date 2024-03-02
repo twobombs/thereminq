01845722efa12eb114f60fcdd90f1da8dbb50a0a
Aryan Blaauw <ablaauw@gmx.net>
2024-03-02 18:20:46 +0100

    Update qrackrng.sh

--- systemscripts/qrackrng.sh
+++ systemscripts/qrackrng.sh
@@ -2,14 +2,16 @@
 #
 # set default device when multigpu; every GPU can generate a stream to be consolidated to /tmp/qrackrng
 export QRACK_OCL_DEFAULT_DEVICE=0
+# 24 qubit qft fluctuations are used as rng feed
 ./benchmarks --optimal --measure-shots=1 -m=24 --samples=999999999 --single --measure-output /tmp/test_qft_cosmology.txt test_qft_cosmology &
 
-# decimal qft fluctuation output converted to binary output
-tail -f /tmp/test_qft_cosmology.txt | xxd -r -p > /tmp/qrackrng &
+# convert decimal qft fluctuation to hex converted binary output
+tail -f /tmp/test_qft_cosmology.txt | hexdump -v -e '1/4 "%08x"' -e '"\n"' | xxd -r -p > /tmp/qrackrng &
 
 # disconnect & setup loop device with qrackrng output 
-losetup -d /dev/qrackrng > /dev/null
-losetup /dev/qrackrng /tmp/qrackrng
+losetup -d /dev/loop99 > /dev/null
+losetup /dev/loop99 /tmp/qrackrng
 
 # tail output of rng device
 tail -f /dev/qrackrng
+#
