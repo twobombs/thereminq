b764de61a186ea82309e61f63c6f1d9aa7623b0c
Aryan Blaauw <ablaauw@gmx.net>
2024-03-02 17:27:52 +0100

    Update qrackrng.sh

--- systemscripts/qrackrng.sh
+++ systemscripts/qrackrng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-./benchmarks --optimal --measure-shots=1024 -m=24 --samples=999999999 --single --measure-output /tmp/test_qft_cosmology.txt test_qft_cosmology &
+./benchmarks --optimal --measure-shots=1 -m=16 --samples=999999999 --single --measure-output /tmp/test_qft_cosmology.txt test_qft_cosmology &
 tail -f /tmp/test_qft_cosmology.txt | xxd -r -p > /tmp/qrackrng &
 losetup /dev/qrackrng /tmp/qrackrng
 
