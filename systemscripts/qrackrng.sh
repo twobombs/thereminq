516aa8ae377f0120da868c399296317f27f09f54
Aryan Blaauw <ablaauw@gmx.net>
2024-03-03 22:18:35 +0100

    Update qrackrng.sh

--- systemscripts/qrackrng.sh
+++ systemscripts/qrackrng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # a qft qrack random number generator written as a PoC in bash
 # three GPUs act as independant sources of entropy feeding to one qrackrng file
-
+#
 # set default device when multigpu; every GPU can generate a stream to be consolidated to /tmp/qrackrng
 
 export QRACK_OCL_DEFAULT_DEVICE=0
@@ -14,12 +14,15 @@ export QRACK_OCL_DEFAULT_DEVICE=2
 # 24 qubit qft fluctuations are used as rng feed
 ./benchmarks --optimal --binary-output --measure-shots=1 -m=24 --samples=999999999 --single --measure-output /tmp/test_qft_cosmology2.txt test_qft_cosmology &
 
+# give the 3 qrack instances to initialize
+sleep 10
 
 # convert decimal qft fluctuations to hex converted binary output
 # tail -f /tmp/test_qft_cosmology.txt | hexdump -v -e '1/4 "%08x"' -e '"\n"' | xxd -r -p /tmp/qrackrng &
-tail -f test_qft_cosmology0.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
-tail -f test_qft_cosmology1.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
-tail -f test_qft_cosmology2.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
+tail -f /tmp/test_qft_cosmology0.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
+tail -f /tmp/test_qft_cosmology1.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
+tail -f /tmp/test_qft_cosmology2.txt | tr -d '\n' | xxd -r -p >> /tmp/qrackrng &
+
 
 # disconnect & setup loop device with qrackrng output 
 #losetup -d /dev/loop99 > /dev/null
