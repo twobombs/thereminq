48c0c7729fe98beff523ffae625c18c079f36c56
Aryan Blaauw <ablaauw@gmx.net>
2024-03-03 11:54:03 +0100

    Update shors_qimcifa_loop.sh

--- runscripts/shors_qimcifa_loop.sh
+++ runscripts/shors_qimcifa_loop.sh
@@ -9,7 +9,7 @@ for i in {65535..4294967295..65535}
 	prime2=$(matho-primes -c 1 -u $i2)
 	echo $(($prime * $prime2)) > fact
 	fact=$(<fact)
-	echo $fact | ./qimcifa $1 1 -1 1 > qimfact
+	echo $fact | ./qimcifa $1 1 1 0 > qimfact
 	cat qimfact | grep 'Found'
 	cat qimfact | grep 'elapsed'
 	cat qimfact | grep 'Bits'
