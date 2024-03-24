d5d06150c9551a2d4fc77cd69eb73318cdc653a6
Aryan Blaauw <ablaauw@gmx.net>
2024-03-24 15:44:55 +0100

    Add files via upload

---
+++ runscripts/run-cosmos-nbody-VCL.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# precompile cl kernels for Qrack	
+#cd /qrack/_build/ && ./qrack_cl_precompile
+
+#export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.40
+#export QRACK_QPAGER_DEVICES=10.3,10.0,10.1,10.2
+#export QRACK_OCL_DEFAULT_DEVICE=3
+
+# default settings for whole run
+export QRACK_QPAGER_DEVICES_HOST_POINTER=8.0,0.1
+export QRACK_MAX_ALLOC_MB=56000
+
+export QRACK_MAX_PAGING_QB=32
+export QRACK_MAX_PAGE_QB=27
+export QRACK_SEGMENT_GLOBAL_QB=6
+
+# run benchmark for load & output simulation
+
+# first testrun on V100 16GB for checks QFT functionality
+export QRACK_OCL_DEFAULT_DEVICE=5
+export QRACK_QPAGER_DEVICES=16.0
+./vclrun ./benchmarks --optimal --layer-qtensornetwork --measure-shots=10000 -m=30 --single --samples=1 --measure-output=/var/log/qrack/"measured_cosmos_$(date +%F_%R_%S).log" test_qft_cosmology
+
+# place default OCL device outside scope of 4 cards
+# each card has 10GB reserved for the state vector
+export QRACK_OCL_DEFAULT_DEVICE=2
+export QRACK_QPAGER_DEVICES=8.0,8.1,8.3,8.5,8.6,8.7
+./vclrun ./benchmarks --optimal --layer-qtensornetwork --measure-shots=10000 -m=32 --single --samples=1 --measure-output=/var/log/qrack/"measured_cosmos_$(date +%F_%R_%S).log" test_qft_cosmology 
+
+# separate run (disabled)
+# export QRACK_OCL_DEFAULT_DEVICE=2
+# export QRACK_QPAGER_DEVICES=10.2
+# ./benchmarks --optimal --layer-qtensornetwork --proc-bdt-hybrid --measure-shots=10000 -m=29 --single --samples=10 --measure-output=/var/log/qrack/"measured_cosmos_$(date +%F_%R_%S).log" test_qft_cosmology 
+
+# M40 24GB legacy run
+export QRACK_OCL_DEFAULT_DEVICE=2
+export QRACK_QPAGER_DEVICES=12.0
+./vclrun ./benchmarks --optimal --layer-qtensornetwork --measure-shots=10000 -m=29 --single --samples=1 --measure-output=/var/log/qrack/"measured_cosmos_$(date +%F_%R_%S).log" test_qft_cosmology 
+
+# sleep 5
+# ./benchmarks --optimal --measure-shots=1024 --proc-stabilizer-qpager -m=27 --device-id=1 --single --samples=1000 --measure-output=/var/log/qrack/"measured_cosmos_$(date +%F_%R_%S).log" test_qft_cosmology
+
+# end run, do not stop container ( debug feature )
+# tail -f /dev/null
